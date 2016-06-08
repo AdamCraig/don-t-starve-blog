80fed72ff5879541388538a7a68fb66c056a3d86
Adam Craig and Ashley Maceli <adam@>
2016-06-08 10:07:21 -0700

    add contact and about routes and update application.js with header and footer

---
+++ app/router.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('contact');
+  this.route('about');
+});
+
+export default Router;
