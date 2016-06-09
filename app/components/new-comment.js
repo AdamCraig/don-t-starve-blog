970a231d129f85aaafef091c8fc8f90a1b6d6a84
Adam Craig and Ashley Maceli <ashley@>
2016-06-09 15:46:26 -0700

    add pick avatar actions to new-comment.js with jQuery to add/remove class

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -1,7 +1,38 @@
 import Ember from 'ember';
 
+var clearAvatarChoice = function() {
+  $("#pickWebber").removeClass("selected-avatar");
+  $("#pickWendy").removeClass("selected-avatar");
+  $("#pickWX").removeClass("selected-avatar");
+  $("#pickWillow").removeClass("selected-avatar");
+}
+
 export default Ember.Component.extend({
   actions: {
+    pickWebber() {
+      clearAvatarChoice();
+      this.set('image', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNJqLjv6JvtTLuWXqgLWnAFNji6nWEky3bfsy2gGWUwky6vSa6Jg");
+      this.set('name', "Webber");
+      $("#pickWebber").addClass("selected-avatar");
+    },
+    pickWendy() {
+      clearAvatarChoice();
+      this.set('image', "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ-AHgm1nwh0HefYSiO4UW2RKwzg5YFHAAX_rFmWcg49M1PBYOrjA");
+      this.set('name', "Wendy");
+      $("#pickWendy").addClass("selected-avatar");
+    },
+    pickWX() {
+      clearAvatarChoice();
+      this.set('image', "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTdRdu7bZA6nbIstLYcSQ1fKgdnsDiWNjNno9Gamv61aEAiX0gi");
+      this.set('name', "WX-78");
+      $("#pickWX").addClass("selected-avatar");
+    },
+    pickWillow() {
+      clearAvatarChoice();
+      this.set('image', "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQo2zeP6I060pnUSTcaleUadffTtoEHcefQNUsY2P4l39y-Z_YiQg");
+      this.set('name', "Willow");
+      $("#pickWillow").addClass("selected-avatar");
+    },
     saveComment() {
       var params = {
         name: this.get('name'),
@@ -11,5 +42,6 @@ export default Ember.Component.extend({
       };
       this.sendAction('saveComment', params);
     }
+
   }
 });
